5c73e08212c432c01b569eb9fd4738ab664b1461
MatiasValencia <matias.valencia@alumnos.ucentral.cl>
2026-04-29 18:22:39 -0400

    lab/ se agrega el segundo ejercicio de la sesión 11, más los ejercicios de la sesión 12

--- sesion1.sql
+++ sesion1.sql
@@ -685,5 +685,78 @@ EXCEPTION
         DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
 END;
 /
+
+-- Ejercicio 2 Sesion 11
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('Ejercicio 1 Sesion 11');
+END;
+/
+CREATE OR REPLACE FUNCTION promedio_total_productos RETURN NUMBER AS
+    v_promedio NUMBER;
+BEGIN
+    SELECT AVG(Precio) INTO v_promedio FROM Productos;
+    RETURN v_promedio;
+END;
+/
+
+-- Consulta con funcion
+SELECT Nombre, Precio FROM Productos
+WHERE Precio < promedio_total_productos();
+
 -- Commit final
 COMMIT;
+
+-- Ejercicio 1 Sesion 12
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('Ejercicio 1 Sesion 12');
+END;
+/
+-- Funcion
+CREATE OR REPLACE FUNCTION calcular_total_con_descuento(p_pedido_id IN NUMBER) RETURN NUMBER AS
+    v_total NUMBER;
+BEGIN
+    SELECT Total INTO v_total FROM Pedidos
+    WHERE PedidoID = p_pedido_id;
+    IF Total > 1000 THEN
+        v_total := v_total - (v_total * 10 / 100);
+    END IF;
+    RETURN v_total;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        RAISE_APPLICATION_ERROR(-20004, 'Pedido con ID ' || p_cliente_id || ' no encontrado.');
+END;
+/
+
+-- Procedimiento
+CREATE OR REPLACE PROCEDURE aplicar_descuento_pedido(p_pedido_id IN NUMBER) AS
+    v_total_nuevo NUMBER;
+BEGIN
+    v_total_nuevo := calcular_total_con_descuento(p_pedido_id);
+    UPDATE Pedidos
+    SET Total = v_total_nuevo
+    WHERE PedidoID = p_pedido_id;
+    DBMS_OUTPUT.PUT_LINE('Total del pedido con ID ' || p_pedido_id || ' actualizado a $' || v_total_nuevo);
+    COMMIT;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        RAISE_APPLICATION_ERROR(-20004, 'Pedido con ID ' || p_cliente_id || ' no encontrado.');
+END;
+/
+
+-- Ejercicio 2 Sesion 12
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('Ejercicio 2 Sesion 12');
+END;
+/
+CREATE OR REPLACE TRIGGER validar_cantidad_detalle
+BEFORE INSERT OR UPDATE ON DetallesPedidos
+FOR EACH ROW
+BEGIN
+    IF :NEW.Cantidad <= 0 THEN
+        DBMS_OUTPUT.PUT_LINE('La cantidad debe ser mayor a 0.');
+    END IF;
+END;
+/
+
+INSERT INTO DetallesPedidos (DetalleID, PedidoID, ProductoID, Cantidad) VALUES (11, 103, 3, -4)
+INSERT INTO DetallesPedidos (DetalleID, PedidoID, ProductoID, Cantidad) VALUES (11, 103, 3, 3)
